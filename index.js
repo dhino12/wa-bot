f78b4143b26ae19eaba0d70d83abb373b70d034b
dhino12 <dhinorahmad0@gmail.com>
2021-09-17 20:34:55 +0700

    create realtime webserver

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const { Client } = require('whatsapp-web.js');
-const { sendStickerFromUrl, sendImageAsSticker } = require('./src/script/item/send-sticker');
 const { msgHandler } = require('./src/script/msgHandler');
-const qrCode = require('qrcode-terminal');
+const qrCode = require('qrcode');
+const socketIO = require('socket.io');
 const fs = require('fs');
-const app = require('express')();
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server)
 
 const SESSION_FILE_PATH = './wa-session.json';
 let sessionCfg;
@@ -14,28 +19,47 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
 
 const client = new Client({ puppeteer: { headless: true }, session: sessionCfg });
 
-client.on('qr', qr => {
-    qrCode.generate(qr);
-    console.log(`QR Received`, qr);
-})
-
-client.on('authenticated', (session) => {
-    console.log('AUTHENTICATED', session);
-    sessionCfg = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
-});
-
-client.on('ready', () => {
-    console.log(`Client is Ready`);
+app.get('/', (req, res) => {
+    res.sendFile('./src/views/index.html', { root: __dirname });
 })
 
 client.on(`message`, message => {
     msgHandler(client, message);
-    const { info } = client;
 })
 
 client.initialize(); 
+
+// Socket IO
+io.on('connection', (socket) => {
+    socket.emit('message', 'Connecting ...')
+
+    client.on('authenticated', (session) => {
+        console.log('authenticated', 'Whatsapp is authenticated!');
+        console.log('message', 'Whatsapp is authenticated!');
+        console.log('AUTHENTICATED', session);
+        sessionCfg = session;
+        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+
+    client.on('qr', qr => {
+        console.log(`QR Received`);
+        qrCode.toDataURL(qr, (err, url) => {
+            socket.emit('qr', url);
+            socket.emit('message', 'QR Code Received');
+        })
+    })
+
+    client.on('ready', () => {
+        console.log(`Client is Ready`);
+        socket.emit('message', 'Whatsapp Ready');
+        socket.emit('ready', 'Whatsapp Ready');
+    })
+})
+
+server.listen(8000, () => {
+    console.log('wa-bot running on : localhost:' + 8000);
+})
