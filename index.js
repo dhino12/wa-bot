677f0b96a7c6286ae51e41e71b240b66fcd54605
dhino12 <dhinorahmad0@gmail.com>
2021-09-21 19:13:54 +0700

    build realtime webserver with socket.io

--- index.js
+++ index.js
@@ -1,88 +1,65 @@
-const {
-    Client
-} = require('whatsapp-web.js');
-const {
-    msgHandler
-} = require('./src/script/msgHandler');
-const qrCode = require('qrcode');
-const socketIO = require('socket.io');
-const fs = require('fs');
-const http = require('http');
+const { create, ev } = require('@open-wa/wa-automate');
+const { msgHandler } = require('./src/script/msgHandler');
+
+const socketIo = require('socket.io');
 const express = require('express');
+const http = require('http');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-const port = process.env.PORT || 8000;
-
-const SESSION_FILE_PATH = './wa-session.json';
-let sessionCfg;
-
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
-
-const client = new Client({
-    puppeteer: {
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process', // <- this one doesn't works in Windows
-            '--disable-gpu'
-        ],
-
-    },
-    session: sessionCfg
-});
+const server = http.createServer(app)
+const io = socketIo(server);
+const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-    res.sendFile('./src/views/index.html', {
+    res.sendFile('/src/views/index.html', {
         root: __dirname
-    });
+    })
 })
 
-client.on(`message`, message => {
-    msgHandler(client, message);
-})
+create({
+    useChrome: true,
+    qrTimeout: 0,
+    authTimeout: 0,
+    chromiumArgs: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu'
+    ],
+}).then(client => start(client));
 
-client.initialize();
+function start(client) {
+    client.onMessage(async message => {
+        msgHandler(client, message);
+    })
+}
 
-// Socket IO
 io.on('connection', (socket) => {
+    // ev.on('Authenticating', )
     socket.emit('message', 'Connecting ...')
 
-    client.on('authenticated', (session) => {
-        console.log('authenticated', 'Whatsapp is authenticated!');
-        console.log('message', 'Whatsapp is authenticated!');
-        console.log('AUTHENTICATED', session);
-        sessionCfg = session;
-        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-            if (err) {
-                console.error(err);
-            }
-        });
+    ev.on('qr.**', async (qr) => {
+        //base64 encoded qr code image
+        console.log(`QR Code Received`);
+        socket.emit('qr', qr);
+        socket.emit('message', 'QR Code Received');
+        
     });
+    
+    ev.on('STARTUP.**', async (data, sessionId) => {
+        if(data==='SUCCESS') {
+            console.log(`${sessionId} wa-bot started!`)
+            socket.emit('message', 'Whatsapp Ready');
+            socket.emit('ready', 'Whatsapp Ready');
+        }
 
-    client.on('qr', qr => {
-        console.log(`QR Received`);
-        qrCode.toDataURL(qr, (err, url) => {
-            socket.emit('qr', url);
-            socket.emit('message', 'QR Code Received');
-        })
-    })
-
-    client.on('ready', () => {
-        console.log(`Client is Ready`);
-        socket.emit('message', 'Whatsapp Ready');
-        socket.emit('ready', 'Whatsapp Ready');
     })
 })
 
 server.listen(port, () => {
-    console.log('wa-bot running on : localhost:' + port);
+    console.log('App Running on port : '+ port);
 })
