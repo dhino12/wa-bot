ace4891d9ab8e4514de2efd0642c08ca4d1561a6
dhino12 <dhinorahmad0@gmail.com>
2021-09-22 22:46:43 +0700

    fix

--- src/script/msgHandler.js
+++ src/script/msgHandler.js
@@ -22,8 +22,6 @@ const {
     spawn
 } = require('child_process');
 
-const ffmpeg = require('fluent-ffmpeg');
-
 const useragentOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
 
 const msgHandler = async (client, message) => {
