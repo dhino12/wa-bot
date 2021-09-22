0aa814a167517276ff71792c8bf7b78f76949b2f
dhino12 <dhinorahmad0@gmail.com>
2021-09-22 22:46:18 +0700

    fix

--- src/script/msgHandler.js
+++ src/script/msgHandler.js
@@ -24,10 +24,6 @@ const {
 
 const ffmpeg = require('fluent-ffmpeg');
 
-const {
-    stickerGifToWebp
-} = require('./item/stickerConverter');
-
 const useragentOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
 
 const msgHandler = async (client, message) => {
