75f7ed3140dbe1bb4b529b4f4208c4fec352f349
Mocha Hotto <dhinorahmad0@gmail.com>
2021-09-30 20:53:31 +0700

    fix quotedMsg

--- src/script/msgHandler.js
+++ src/script/msgHandler.js
@@ -34,7 +34,7 @@ const {
 const useragentOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
 
 const msgHandler = async (client, message) => {
-    const {
+    let {
         from,
         body,
         quotedMsg,
@@ -45,12 +45,21 @@ const msgHandler = async (client, message) => {
         isMedia
     } = message;
 
+    // if (!caption.search('/') && !validateUrl(caption)) return;
 
     const commands = caption || body;
     const command = commands.toLowerCase().split(' ')[0];
     const arg = commands.split(' ')[1];
     const optionFormat = commands.split(' ')[2]; // format video = mp4, webm
     const optionSize = commands.split(' ')[3]; // size vide = 360p, 480p, 720p, 1080p,
+    let dataMessage = undefined;
+    if (quotedMsg) {
+        dataMessage = quotedMsg;
+        mimetype = dataMessage.mimetype;
+        duration = dataMessage.duration;
+    } else {
+        dataMessage = message
+    }
 
     switch (command) {
         case onlyCommands['/hi']:
@@ -63,8 +72,16 @@ const msgHandler = async (client, message) => {
             break;
 
         case onlyCommands['/stiker']:
-            if (quotedMsg === '' || isMedia) {
-                const mediaData = await decryptMedia(message, useragentOverride);
+            if (validateUrl(arg)) {
+                await client.sendStickerfromUrl(from, arg, {
+                    author: '',
+                    circle: false,
+                    keepScale: true
+                })
+            }
+
+            if (isMedia) {
+                const mediaData = await decryptMedia(dataMessage, useragentOverride);
                 const imgBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`;
                 await client.sendImageAsSticker(from, imgBase64, {
                     author: '',
@@ -73,20 +90,10 @@ const msgHandler = async (client, message) => {
                 });
             }
 
-            if (quotedMsg) {
-                const mediaData = await decryptMedia(quotedMsg, useragentOverride);
-                const imgBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`;
-                console.log(quotedMsg);
-                await client.sendImageAsSticker(from, imgBase64)
-            }
-
-            if (validateUrl(arg)) {
-                await client.sendStickerfromUrl(from, arg)
-            }
             break;
 
         case onlyCommands['/stiker-nobg']:
-            const mediaData = await decryptMedia(message, useragentOverride);
+            const mediaData = await decryptMedia(dataMessage, useragentOverride);
             const base64img = `data:${mimetype};base64,${mediaData.toString('base64')}`;
             const outputFile = './media/image/noBg.png';
             const dirPath = './media/image';
@@ -109,7 +116,7 @@ const msgHandler = async (client, message) => {
                 circle: false,
                 keepScale: true
             });
-
+            rmSync(outputFile);
             // nonaktif untuk menyimpan gambar yang di remove backgroundnya
             // await fs.writeFile(outputFile, result.base64img, (err) => {
             //     if(err) throw err
@@ -119,53 +126,56 @@ const msgHandler = async (client, message) => {
 
         case onlyCommands['/stiker-gif']:
             if (mimetype === 'video/mp4' && duration <= 10) {
-                const md = await decryptMedia(message, useragentOverride);
+                const md = await decryptMedia(dataMessage, useragentOverride);
                 try {
                     const fileBuffer = `data:${mimetype};base64,${md.toString('base64')}`;
-                    const result = await client.sendMp4AsSticker(from, fileBuffer, {
-                        crop: false
-                    }, {
-                        keepScale: true
+                    const result = await client.sendMp4AsSticker(from, fileBuffer, { crop: false }, {
+                        keepScale: true 
                     });
-                    if (result) {
-                        console.log(message);
-                    }
                 } catch (error) {
                     console.log(error.data);
-                    client.reply(message.to, 'File membutuhkan waktu terlalu lama, bisa diulangi kembali', message.id);
+                    await client.reply(from, 'File membutuhkan waktu terlalu lama, bisa diulangi kembali', id);
                 }
-            } else {}
+            }
+
+            if (validateUrl(arg)) {
+
+            }
             break;
 
         case onlyCommands['/yt']:
             console.log('start');
             if (validateUrl(arg)) {
-                // ytDownloader(arg)
+                ytDownloader(arg)
             }
-        break;
+            break;
 
         case onlyCommands['/help']:
             const allCommands = Object.keys(desc).map((command, i) => `*${command}* : ${Object.values(desc)[i]}\n`);
             let strCommand = '======= Perintah untuk bot =======\n' + '=============================\n'
             strCommand += replaceAll(allCommands.toString(), ',', '')
             await client.sendText(from, strCommand);
-            break; 
+            break;
     }
 }
 
-async function ytDownloader(link, quality = 720) {
+async function ytDownloader(link, quality = '720p') {
     const info = await ytdl.getBasicInfo(link);
     console.log(info);
-    const higher = info.formats.filter(item => item.height === quality && item.audioQuality !== undefined)[0];
-    const filePath = `./media/tmp/video/yt.${higher.mimeType.split((';'))[0].split('/')[1]}`
-    ytdl(link, {
-            quality: higher.itag
-        })
-        .pipe(createWriteStream(filePath))
-        .on('end', async () => {
-            // await client.sendFile(from, '');
-            // rmSync(filePath);
-        })
+    if(info.videoDetails.lengthSeconds <= 1800){
+        const higher = info.formats.filter(item => item.qualityLabel === quality && item.audioQuality !== undefined)[0];
+        const filePath = `./media/tmp/video/yt.${higher.mimeType.split((';'))[0].split('/')[1]}`
+        ytdl(link, {
+                quality: higher.itag
+            })
+            .pipe(
+                // createWriteStream(filePath)
+            )
+            // .on('end', async () => {
+            //     // await client.sendFile(from, filePath, );
+            //     // rmSync(filePath);
+            // })
+    }
 }
 
 module.exports = {
