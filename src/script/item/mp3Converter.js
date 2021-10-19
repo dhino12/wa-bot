0e20dd02022a602de0bd7fecc11babb7d6a8bc85
dhino12 <dhinorahmad0@gmail.com>
2021-10-19 12:37:53 +0700

    add progress convert

--- src/script/item/mp3Converter.js
+++ src/script/item/mp3Converter.js
@@ -1,17 +1,30 @@
 const ffmpeg = require('fluent-ffmpeg')();
+let progress = ``;
 
-function toMp3(filePath, fileOut) {
+function toMp3(filePath, fileOut, arg) {
+
+  if (arg === 'info') {
+    return `Proses convert : *${progress}%* `;
+  }
+  
+  return new Promise((resolve, reject) => {
     ffmpeg
-        .input(filePath)
-        .on('progress', (info) => {
-            console.log(`Progress : ${info.percent} %`);
-        })
-        .output(fileOut)
-        .on('end', function() {
-          console.log('Finished processing');
-        })
-        .on('error', (err) => { console.log(err); })
-        .run();
+      .input(filePath)
+      .on('progress', (info) => {
+          console.log(`Progress : ${info.percent} %`);
+          progress = `${info.percent} % convert`
+      })
+      .output(fileOut)
+      .on('error', (err) => { 
+        console.log(err); 
+        reject(err)
+      })
+      .on('end', () => {
+        console.log('Finished processing');
+        resolve('finish');
+      }) 
+      .run();
+  })
 }
 
 module.exports = { toMp3 }
